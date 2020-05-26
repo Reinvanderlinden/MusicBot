65bdd9f848a92e2ad98a95aa002a2e73653efd68
Joost van der Linden <48421694+Reinvanderlinden@users.noreply.github.com>
2020-05-26 04:38:22 +0200

    Update main.cpp

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,6 +1,6 @@
 //Laptop
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <zmq.hpp>
 #include <iostream>
 
@@ -10,7 +10,6 @@ void * context; //Global context, because you only need one !
 
 int main( int argc, char * argv[] )
 {
-    printf("Started\n");
     zmq::context_t ctx;
 
     zmq::socket_t pusher(ctx, zmq::socket_type::push);
@@ -39,18 +38,32 @@ int main( int argc, char * argv[] )
 
     strcat(temp_string_3,temp_char);
     strcat(temp_string_3,">");
-    cout << temp_string_3 << endl;
+    //cout << temp_string_3 << endl;
 
     size_t t = (strlen(temp_string_3)+1);
     zmq::message_t send;
     send = zmq::message_t(temp_string_3, t);
     pusher.send(send, zmq::send_flags::none);
 
-    printf("listening\n");
+    printf("Response...\n");
+
     zmq::message_t request;
-    //receive a request from client
     subscriber.recv(request, zmq::recv_flags::none);
-    cout << "Received " << request.to_string() << endl;
+
+    string out_string = request.to_string();
+
+    char out_char[1000] = " ";
+    out_string.copy(out_char, out_string.size() + 1);
+    out_char[out_string.size()] = '\0';
+
+    out_char[strlen(out_char)-1] = '\0';
+   char *out_char_2  = strrchr(out_char,'>');
+   for(size_t i= 1; i <= strlen(out_char_2);i++)
+   {
+       out_char_2[i-1] = out_char_2[i];
+   }
+
+    cout << out_char_2 << endl;
 
 
     goto loop;
